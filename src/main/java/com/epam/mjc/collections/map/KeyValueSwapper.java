f3e88d0ce0e4dda0eb36ddd8bf07a06aea62036b
VladimirKameko <drugs1360@gmail.com>
2023-05-20 20:26:33 +0300

    First commit

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,13 @@
 package com.epam.mjc.collections.map;
 
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+
+        return new HashMap<>(sourceMap)
+                .entrySet().stream()
+                .collect(Collectors.toMap(Map.Entry::getValue, Map.Entry::getKey, (a, b) -> a));
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,17 @@
 package com.epam.mjc.collections.map;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        if (sentence.length() == 0) {
+            return new HashMap<>();
+        }
+        String[] res = sentence.trim().toLowerCase().replaceAll("\\p{IsPunctuation}", "").split(" ");
+        return Arrays.stream(res)
+                .collect(Collectors.toMap(e -> e, e -> 1, Integer::sum));
     }
 }
